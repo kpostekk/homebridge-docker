690e516d199aea43fea275b23e2e42c1408b18fb
Krystian Postek <krystian@postek.eu>
2021-04-11 16:17:44 +0200

    Fix platform register

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import {API} from 'homebridge'
-import { PLATFORM_NAME } from './settings'
+import {PLATFORM_NAME, PLUGIN_NAME} from './settings'
 import { DockerHomebridgePlatform } from './platform'
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, DockerHomebridgePlatform)
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, DockerHomebridgePlatform)
 }
