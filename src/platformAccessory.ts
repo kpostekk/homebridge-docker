0a7e8179f3d186018a52abbe1882d204902535e1
Krystian Postek <krystian@postek.eu>
2021-04-13 11:02:48 +0200

    Add miss click preventing ✨feature✨

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -6,9 +6,12 @@ import {DockerHomebridgePlatform} from './platform'
 export class DockerPlatformAccessory {
   private service: Service
   private readonly log: Logger
+  private readonly secureDelayTimeout?: number
 
   private states = {
     On: false,
+    SecureDelay: false, // indicates
+    SecureDelayMissClickDetection: true,
   }
 
   constructor(
@@ -17,6 +20,11 @@ export class DockerPlatformAccessory {
   ) {
     // copy logging
     this.log = this.platform.log
+
+    // read secure delay settings
+    if (this.platform.config.SecureDelayTimeout > 0) {
+      this.secureDelayTimeout = this.platform.config.secureDelayTimeout
+    }
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
@@ -36,8 +44,44 @@ export class DockerPlatformAccessory {
    * Set status of container
    * @param value
    */
-  setOn(value: CharacteristicValue) {
-    if (value) {
+  async setOn(value: CharacteristicValue) {
+    // Add some log readability
+    this.log.debug(`Performing action for ${this.accessory.context.device.Name}`)
+
+
+    // Secure delay border
+    if (this.secureDelayTimeout) {
+      if (this.states.SecureDelay) {
+        // When last previous secure delay hasn't passed
+        this.log.debug('Secure delay has been already triggered, mocking change, setting miss click flag and return')
+        this.states.On = !this.states.On // switch state
+        this.states.SecureDelayMissClickDetection = true
+        return
+      } else {
+        // When secure delay wasn't detected
+        this.log.debug('No secure delay, triggering one')
+        this.states.SecureDelay = true // change state
+        this.states.On = !this.states.On // switch state
+      }
+
+      // performing delay
+      this.log.debug(`Performing delay (${this.secureDelayTimeout!})`)
+      await new Promise(resolve => setTimeout(resolve, this.secureDelayTimeout!))
+
+      // Disable secure delay lock
+      this.log.debug('Disabling secure delay lock')
+      this.states.SecureDelay = false
+
+      // Check miss click flag
+      if (this.states.SecureDelayMissClickDetection) {
+        this.log.debug('Miss click detected, clearing flag and return')
+        this.states.SecureDelayMissClickDetection = false
+        return
+      }
+    }
+
+
+    // Main switch mechanism
     if (value) { // start container
       this.log.debug('Starting...')
       this.platform.docker.getContainer(
@@ -67,6 +111,11 @@ export class DockerPlatformAccessory {
    * Get status of container
    */
   async getOn(): Promise<CharacteristicValue> {
+    if (this.states.SecureDelay) {
+      this.log.debug(`Found secure delay lock, mocking state of ${this.accessory.context.device.Name}`)
+      return this.states.On
+    }
+
     const inspection = await this.platform.docker.getContainer(
       this.accessory.context.device.Id,
     ).inspect()
